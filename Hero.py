f41bd0c6e9b71f70c06e7d4ad0584583cde8c78a
Aktivv <e.n.d.kgkg@gmail.com>
2024-06-07 02:15:29 +0600

    HW_2

--- Hero.py
+++ Hero.py
@@ -20,9 +20,55 @@ class SuperHero:
         self.health_points *= 2
 
 
-Hero = SuperHero('Sky', 'Skii', 'Fly', 200, 'Oops')
-Hero.get_name()
-Hero.double_health()
+class Air(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        SuperHero.__init__(self, name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def __str__(self):
+        return f'{self.nickname} {self.superpower} {self.health_points} {self.damage} {self.fly}'
+
+    def double_health(self):
+        self.health_points **= 2
+        self.fly = True
+
+    def phrase(self):
+        print('True in the True_phrase')
+
+
+class Earth(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def __str__(self):
+        return f'{self.nickname} {self.superpower} {self.health_points} {self.damage} {self.fly}'
+
+    def double_health(self):
+        self.health_points **= 2
+        self.fly = True
+
+    def phrase(self):
+        print('True in the True_phrase')
+
+
+class Vilian(Earth):
+    people = 'monster'
+
+    def gen_x(self):
+        pass
+
+    def crit(self):
+        self.damage **= 2
+
+
+Hero = Earth('Grount', 'Grock', 'Shield', 1000, 'GROOOCK!!!', 5)
+Hero2 = Air('Sky', 'Skii', 'Fly', 200, 'Pew', 20)
 print(Hero)
-print(Hero.health_points)
-print(len(Hero))
+Hero2.double_health()
+print(Hero2)
+Hero.phrase()
+'''И как жто должно было работать?'''
+print(Vilian.crit(Hero2))
